3bbd9f74361bdbc4a49e930a101b7d1f24224ab6
Adityabhaliya <adityabhaliyamaruti@gmail.com>
2025-02-13 23:37:53 +0530

    change

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
 // 6. routes/userRoutes.js
 const express = require('express');
-const { registerUser, loginUser, sendOtp, adminLogin, welcome ,uploadImage, forgetpassword, resetPassword} = require('../controller/user.controller');
+const { registerUser, loginUser, sendOtp, adminLogin, welcome ,uploadImage,  resetPassword, forgetPassword} = require('../controller/user.controller');
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ router.get('/welcome', welcome);
 router.post('/register', registerUser);
 router.post('/login', loginUser); 
 router.post('/admin-login', adminLogin); 
-router.post('/forgetpassword', forgetpassword);  
+router.post('/forgetpassword', forgetPassword);  
 router.post('/reset-password', resetPassword); 
 router.post('/images-upload', uploadImage);
 
