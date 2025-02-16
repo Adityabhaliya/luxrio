a787179766553782e6d83387c9c1ef1b75c0bdc1
Adityabhaliya <adityabhaliyamaruti@gmail.com>
2025-02-16 23:14:29 +0530

    dd

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const userRoutes = require('./user.routes');
 const productRoutes = require('./product.routes');
 const categoryRoutes = require('./category.routes');
+const blogRoutes = require('./blog.routes');
 
 const router = express.Router();
 router.use('/', userRoutes);
 router.use('/', categoryRoutes);
 router.use('/', productRoutes);
+router.use('/', blogRoutes);
 
 module.exports = router;
