8c7a6716336d4c9ea2631f2c91c40661be53bc55
Adityabhaliya <adityabhaliyamaruti@gmail.com>
2025-04-03 23:36:59 +0530

    dd

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -8,7 +8,8 @@ router.post('/admin/blog-create', verifyAdminToken, createblog);
 router.put('/admin/blog-edit/:slug', verifyAdminToken, editblog);
 router.delete('/admin/blog/:slug', verifyAdminToken, deleteblog);
 router.get('/admin/blog-list', verifyAdminToken,listblogsPagination);
-router.get('/user/blog-list',listblogsPaginationUser);
+router.get('/user/blog-list',listblog);
+router.get('/user/blog-list/:slug', getblogBySlug);
 router.get('/admin/blog-list/:slug',verifyAdminToken, getblogBySlug);
 router.put('/admin/blog-block', verifyAdminToken ,adminblogBlock);
 
