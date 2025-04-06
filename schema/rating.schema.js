4dc4183c95d4659b40ccd2099ba4e6154a838170
Adityabhaliya <adityabhaliyamaruti@gmail.com>
2025-04-06 16:43:41 +0530

    dd

--- schema/rating.schema.js
+++ schema/rating.schema.js
@@ -4,7 +4,8 @@ const sequelize = require('../db/db.config');
 
 const ratingSchema = sequelize.define('ratings', {
     user_id: { type: DataTypes.INTEGER, allowNull: false },
-    product_id: { type: DataTypes.JSON, allowNull: false }, // Store multiple product IDs as JSON array
+    product_id: { type: DataTypes.INTEGER, allowNull: false }, // Store multiple product IDs as JSON array
+    order_id: { type: DataTypes.INTEGER, allowNull: false }, // Store multiple product IDs as JSON array
     description: { type: DataTypes.TEXT, allowNull: false },
     rating: { type: DataTypes.FLOAT, allowNull: false }, // INR or USD
     
