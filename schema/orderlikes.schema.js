8032e4cf63f2247ffd0d5d860f56fc926fa81827
Adityabhaliya <adityabhaliyamaruti@gmail.com>
2025-04-08 23:17:42 +0530

    qss

--- schema/orderlikes.schema.js
+++ schema/orderlikes.schema.js
@@ -6,7 +6,7 @@ const sequelize = require('../db/db.config');
 
 const orderlikes = sequelize.define('orderlikes', {
     user_id: { type: DataTypes.INTEGER, allowNull: false },
-    order_id: { type: DataTypes.INTEGER, allowNull: false },  
+    rating_id: { type: DataTypes.INTEGER, allowNull: false },  
     is_like: { type: DataTypes.BOOLEAN, defaultValue: false },
     is_unlike: { type: DataTypes.BOOLEAN, defaultValue: false } 
 }, {
